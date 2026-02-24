a57ee1a74a0a386aad534b3715cc3aa7fe27fb2e
Gary Ramsey <garyramsey@Garys-MacBook-Pro.local>
2026-02-23 21:22:42 -0600

    Fix next-pwa config for Next 16

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
+import withPWAInit from "next-pwa";
 
-const isDev = process.env.NODE_ENV === "development";
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require("next-pwa")({
+const withPWA = withPWAInit({
   dest: "public",
-  disable: isDev, // don't generate SW in dev to avoid caching headaches
+  disable: process.env.NODE_ENV === "development",
 });
 
-const nextConfig: NextConfig = {
-  // If you run into build issues later, we can adjust settings here.
-};
+const nextConfig: NextConfig = {};
 
 export default withPWA(nextConfig);
