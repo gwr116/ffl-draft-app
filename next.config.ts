bf3cacaa839f4bea81eb72c8a50b723f9b8bd2ab
Gary Ramsey <garyramsey@Garys-MacBook-Pro.local>
2026-02-23 21:16:02 -0600

    Add PWA support (manifest + service worker)

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
+const isDev = process.env.NODE_ENV === "development";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: isDev, // don't generate SW in dev to avoid caching headaches
+});
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  // If you run into build issues later, we can adjust settings here.
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
